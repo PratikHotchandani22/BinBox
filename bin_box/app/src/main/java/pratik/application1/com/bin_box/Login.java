fd24caf46233b22d930cf57e6e249188c4bd2ecf
PratikHotchandani22 <pratikhotchandani22@gmail.com>
2019-09-07 17:14:06 +0530

    Commit 2

--- bin_box/app/src/main/java/pratik/application1/com/bin_box/Login.java
+++ bin_box/app/src/main/java/pratik/application1/com/bin_box/Login.java
@@ -2,12 +2,43 @@ package pratik.application1.com.bin_box;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
+
+
+    EditText PhNumber, Name;
+    ImageButton BtnContinue;
+
+    user us;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        PhNumber = (EditText)findViewById(R.id.PhNumber);
+        Name = (EditText)findViewById(R.id.Name);
+        BtnContinue = (ImageButton)findViewById(R.id.Conti);
+
+
+        BtnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = Name.getText().toString().trim();
+                String PNo =  PhNumber.getText().toString().trim();
+                us = new user(name,PNo);
+
+
+            }
+        });
     }
 }
---
+++ bin_box/app/src/main/java/pratik/application1/com/bin_box/user.java
@@ -0,0 +1,27 @@
+package pratik.application1.com.bin_box;
+
+public class user {
+    String name;
+    String pno;
+
+    public user(String name, String pno) {
+        this.name = name;
+        this.pno = pno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPno() {
+        return pno;
+    }
+
+    public void setPno(String pno) {
+        this.pno = pno;
+    }
+}
